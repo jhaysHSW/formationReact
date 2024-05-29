7f3530637d534996394a3d15f7071ff16e3694d0
jhays <jhays@horizontalsoftware.com>
2024-05-29 13:53:49 +0200

    ex7 v2

--- 07_destructuration.js
+++ 07_destructuration.js
@@ -12,7 +12,8 @@
  *  - interdiction d'utiliser slice ou splice
  */
 
-const extractFirstTwo = (array) => { let [arg1, arg2] = array; return [arg1, arg2]; }
+//const extractFirstTwo = (array) => { let [arg1, arg2] = array; return [arg1, arg2]; }
+const extractFirstTwo = ([arg1, arg2]) => [arg1, arg2]
 
 console.log("extractFirstTwo([1, 2, 3]) : " + extractFirstTwo([1, 2, 3]));
 
@@ -23,7 +24,8 @@ console.log("extractFirstTwo([1, 2, 3]) : " + extractFirstTwo([1, 2, 3]));
  * exemple: [1, 2, 3] => [2, 3]
  */
 
-const extractRest = (array) => { let [arg1, ...args] = array; return args; }
+//const extractRest = (array) => { let [arg1, ...args] = array; return args; }
+const extractRest = ([arg1, ...args]) => args
 
 console.log("extractRest([1, 2, 3]) : " + extractRest([1, 2, 3]));
 
@@ -39,7 +41,8 @@ console.log("extractRest([1, 2, 3]) : " + extractRest([1, 2, 3]));
  * - interdiction d'utiliser l'opérateur "." pour accéder au champ "name"
  */
 
-const extractName = (obj) => { let {name, age} = obj; return name; }
+//const extractName = (obj) => { let {name, ...rest} = obj; return name; }
+const extractName = ({name}) => name
 
 console.log("extractName({name: toto, age: 42}) : " + extractName({name: "toto", age: 42}));
 
@@ -54,7 +57,8 @@ console.log("extractName({name: toto, age: 42}) : " + extractName({name: "toto",
  * 
  */
 
-const removePassword = (obj) => {let {name, password} = obj; return {name : name};};
+//const removePassword = (obj) => {let {password, ...rest} = obj; return rest; };
+const removePassword = ({password, ...rest}) => rest
 
 console.log("removePassword({name: toto, password: 1234}).password : " + removePassword({name: "toto", password: "1234"}).password);
 
