4d667d982282036934fb140ebee7a0a27124aa39
jhays <jhays@horizontalsoftware.com>
2024-05-29 15:54:58 +0200

    ex12

--- 12_parallel.js
+++ 12_parallel.js
@@ -13,10 +13,18 @@
  * documentation: https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
 */
 
-//const {apiResponse} = require("./11_async");
+const {apiResponse} = require("./11_async");
 
 const parallel = () => {
+    const promise1 = apiResponse("https://jsonplaceholder.typicode.com/todos/1");
+    const promise2 = apiResponse("https://jsonplaceholder.typicode.com/todos/2");
+    const promise3 = apiResponse("https://jsonplaceholder.typicode.com/todos/3");
 
+    Promise.all([promise1, promise2, promise3]).then((values) => {
+        return values;
+    });
 }
+
+parallel();
 
 module.exports = {parallel};
